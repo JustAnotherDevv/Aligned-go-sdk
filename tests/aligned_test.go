6c56af14bc246b9cf8d816926d5b92f9aad2fd07
JustAnotherDevv <daniel_zarzecki@wp.pl>
2024-07-09 11:35:55 +0200

    test file

---
+++ tests/aligned_test.go
@@ -0,0 +1,68 @@
+package aligned_test
+
+import (
+	"crypto/ecdsa"
+	"io/ioutil"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/stretchr/testify/assert"
+
+	"aligned"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	proofGeneratorAddress = "0x66f9664f97F2b50F62D13eA064982f936dE76657"
+	privateKeyHex         = "0x7d2647ad2e1f6c1dce5abe2b5c3b9c8ecfe959e40b989d531bbf6624ff1c62df"
+)
+
+func loadFile(path string) ([]byte, error) {
+	return ioutil.ReadFile(path)
+}
+
+func getPrivateKey() (*ecdsa.PrivateKey, error) {
+	return crypto.HexToECDSA(privateKeyHex[2:])
+}
+
+func getRpcClient() (*rpc.Client, error) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file:", err)
+	}
+
+	apiKey := os.Getenv("HOLESKY_INFURA")
+	return rpc.Dial(apiKey)
+}
+
+func TestSubmitMultipleGroth16(t *testing.T) {
+	proof, err := loadFile("test_files/groth16_bn254/plonk.proof")
+	assert.NoError(t, err)
+
+	vk, err := loadFile("test_files/groth16_bn254/plonk.vk")
+	assert.NoError(t, err)
+
+	pub, err := loadFile("test_files/groth16_bn254/plonk_pub_input.pub")
+	assert.NoError(t, err)
+
+	privateKey, err := getPrivateKey()
+	assert.NoError(t, err)
+
+	client, err := getRpcClient()
+	assert.NoError(t, err)
+
+	groth16Data := aligned.VerificationData{
+		ProvingSystem:         aligned.Groth16Bn254,
+		Proof:                 proof,
+		PublicInput:           pub,
+		VerificationKey:       vk,
+		VMProgramCode:         nil,
+		ProofGeneratorAddress: proofGeneratorAddress,
+	}
+
+	println("test ")
+}
